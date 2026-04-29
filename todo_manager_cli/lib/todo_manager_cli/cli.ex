2a52d353f83f56c004f33ab3812a0ad62b3d9654
Mariana Rocha <m.rocha42@outlook.com>
2026-04-29 02:24:25 -0300

    fix: fixing invalid menu option return

--- todo_manager_cli/lib/todo_manager_cli/cli.ex
+++ todo_manager_cli/lib/todo_manager_cli/cli.ex
@@ -8,19 +8,23 @@ defmodule TodoManagerCli.Cli do
     option =
       IO.gets("What do you want to do? Options(1, 2, 3, 4):\n")
       |>String.trim
-      |>String.to_integer
     handle_option(option, tasks)
   end
-  def handle_option(option, tasks) do
-    case option do
-      1 -> add_task(tasks)
-      2 -> list_tasks(tasks)
-      3 -> remove_task(tasks)
-      4 -> exit()
-      _ ->
-        IO.puts("Invalid option, please try again.")
-        menu(tasks)
-    end
+  def handle_option("1", tasks) do
+    add_task(tasks)
+  end
+  def handle_option("2", tasks) do
+    list_tasks(tasks)
+  end
+  def handle_option("3", tasks) do
+    remove_task(tasks)
+  end
+  def handle_option("4", _tasks) do
+    IO.puts("Bye!")
+  end
+  def handle_option(_, tasks) do
+    IO.puts("Invalid option")
+    menu(tasks)
   end
   def add_task(tasks) do
     newtask =
